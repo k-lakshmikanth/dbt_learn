84586d6a7069dcc123c7ef7c101ae943517c408b
lakshmikanth kasapuram <lkadmin@8pp3cg.onmicrosoft.com>
2024-07-19 08:51:58 +0530

    dev

--- models/enriched/EU_orders.sql
+++ models/enriched/EU_orders.sql
@@ -2,4 +2,14 @@ with orders as (
     select * from {{ ref("RAW_orders") }}
 )
 
-select * from orders
+select 
+o_orderkey as orderkey,
+o_custkey as customerkey,
+o_orderstatus as orderstatus,
+o_totalprice as totalprice,
+o_orderdate as orderdate,
+o_orderpriority as orderpriority,
+o_clerk as clerk,
+o_shippriority as shippriority,
+o_comment as comment
+ from raw.raw_orders
